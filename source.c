1469351ad7e88d66629610e8e3372bb2a144af9c
ExecutionerSK7 <sawfal24@gmail.com>
2021-10-20 17:51:11 +0545

    added add record function

--- source.c
+++ source.c
@@ -8,9 +8,9 @@
 struct Record{
 	char name[30];
 	char id[10];
-	char weight[7];
-	char height[7];
-	char gender[8];
+	char weight[10];
+	char height[10];
+	char gender[10];
 	struct DOB{
 		int year;
 		int month;
@@ -25,7 +25,7 @@ struct Record{
 
 void loadscreen(int time);
 void login();
-//void addrecord();
+void addrecord();
 //void searchrecord();
 //void editrecord();
 void viewrecord();
@@ -38,8 +38,7 @@ void main()
 	system("cls");
 	mkdir("file");
 	loadscreen(100);
-	printf("\n\nEnter any key to continue........");
-	_getch();
+	login();
 	system("cls");
 
 	int ch;
@@ -68,9 +67,9 @@ void main()
 			viewrecord();
 			break;
 
-//		case 2:
-//			addrecord();
-//			break;
+		case 2:
+			addrecord();
+			break;
 /*
 		case 3:
 			editrecord();
@@ -87,13 +86,13 @@ void main()
 		case 6:
 			system("cls");
 			printf("\n\n\t\tThank you for using PRMS. Your program will now exit.\n\n ");
-			_getch();
+			getch();
 			exit(0);
 
 		default:
 			printf("\nYOU ENTERED WRONG CHOICE.");
 			printf("\nPRESS ANY KEY TO TRY AGAIN");
-			_getch();
+			getch();
 			break;
 
 		}
@@ -101,7 +100,6 @@ void main()
 
 	}
 
-	login();
 }
 
 void loadscreen(int time)
@@ -131,8 +129,7 @@ void login()
 	int atmpt = 0, i = 0;
 	char user[10], c = ' ';
 	char pword[10], code[10];
-	//char userid[10] = "user";
-	//char pass[10] = "pass";
+
 	do
 	{
 		printf("\n\nWelcome to Police Record Management System. Please enter your login info:\n\n");
@@ -142,7 +139,7 @@ void login()
 		printf(" \n                       ENTER PASSWORD:-");
 		while (i < 10)
 		{
-			pword[i] = _getch();
+			pword[i] = getch();
 			c = pword[i];
 			if (c == 13) break;
 			else printf("*");
@@ -156,7 +153,7 @@ void login()
 		{
 			printf("  \n\n\n    LOGIN SUCCESSFUL!! ");
 			printf("\n\n\n\t\t\tEnter any key to continue...");
-			_getch();//holds the screen
+			getch();//holds the screen
 			break;
 		}
 		else
@@ -164,7 +161,7 @@ void login()
 			printf("\n        SORRY !!!!  LOGIN IS UNSUCESSFUL");
 			atmpt++;
 
-			_getch();//holds the screen
+			getch();//holds the screen
 			system("cls");
 
 		}
@@ -173,153 +170,150 @@ void login()
 	if (atmpt > 2)
 	{
 		printf("\nYou have exceeded the attempt limit! Contact your technical provider!! ");
-		_getch();
+		getch();
 
 	}
 	system("cls");
 }
-/*
+
 void addrecord()
+
 {
 	system("cls");
-	char Y = 'y';
-	fflush(stdin);
-	char id[10];
-	char filename[10];
-	int exists = 0;
+	char Y = 'Y', id[10];
+	char filename[30];
+	int exists;
 
 	printf("\n\n\t\t====================================\n");
 	printf("\t\t\t  - ADD RECORDS -");
 	printf("\n\t\t====================================\n");
-	printf("\n\n\tENTER FIRST NAME OF CONVICT(To Check whether record exists or not): ");
-	fflush(stdin);
-	gets(filename);
 
-	fp = fopen("file", "ab+");
 
-	if (fp == NULL) {
-		fp = fopen("file", "wb+");
-		printf("Error while opening record file. Contact your technical team\n");
-		printf("Enter any key to exit.....");
-		_getch();
-		return;
+	fp = fopen("filename", "ab+");
+
+	if (fp == NULL)
+	{
+		fp = fopen("filename", "wb+");
+		if (fp == NULL)
+		{
+			printf("\nSYSTEM ERROR...");
+			printf("\nPRESS ANY KEY TO EXIT");
+			getch();
+			return;
+		}
 	}
 
 	while (Y == 'Y' || Y == 'y')
+
 	{
 		exists = 0;
-		fflush(stdin);
-		printf("\nEnter convict's id: ");
+		fseek(stdin, 0, SEEK_END);
+
+		printf("\tENTER CONVICT CODE(To check whether it matches or not): ");
 		scanf("%s", id);
-		fflush(stdin);
 
 		rewind(fp);
+
 		while (fread(&record, sizeof(record), 1, fp) == 1)
 		{
 			if (strcmp(record.id, id) == 0)
 			{
-				printf("Record already exists.\n");
+
+				printf("\n\tTHE RECORD ALREADY EXISTS.\n");
 				exists = 1;
+
 			}
+
 		}
 
-		if (exists == 0) {
+		if (exists == 0)
+		{
+
 			strcpy(record.id, id);
-			fflush(stdin);
 
-			printf("Enter name of convict: flushhed");
-			fflush(stdin);
+			printf("\n\tENTER NAME OF CONVICT: ");
+			fseek(stdin, 0, SEEK_END);
 			gets(record.name);
-			fflush(stdin);
 
-			printf("Enter weight: ");
-			fflush(stdin);
-			gets(record.weight);
-			fflush(stdin);
-
-			printf("Enter height: ");
-			fflush(stdin);
-			gets(record.height);
-			fflush(stdin);
-
-			printf("Enter Date of birth: \n");
+			printf("\tENTER SEX: ");
+			gets(record.gender);
+			fseek(stdin, 0, SEEK_END);
+	
+			printf("Enter date of birth...\n");
 			printf("Enter year: ");
 			scanf("%d", &record.dob.year);
 			printf("Enter month: ");
 			scanf("%d", &record.dob.month);
 			printf("Enter day: ");
 			scanf("%d", &record.dob.day);
-			fflush(stdin);
-			
-			printf("Enter hair color: ");
-			fflush(stdin);
-			gets(record.hair);
-			fflush(stdin);
 
-			printf("Enter eye color: ");
+			printf("\tENTER WEIGHT: ");
+			gets(record.weight);
+			fseek(stdin, 0, SEEK_END);
+
+			printf("\tENTER HEIGHT(FT): ");
+			gets(record.height);
+			fseek(stdin, 0, SEEK_END);
+
+			printf("\tENTER HAIRCOLOR: ");
+			gets(record.hair);
+			fseek(stdin, 0, SEEK_END);
+
+			printf("\tENTER EYECOLOR: ");
 			gets(record.eye);
 			fflush(stdin);
 
-			printf("Enter crime committed: ");
-			fflush(stdin);
+			printf("\tENTER CRIME: ");
 			gets(record.crime);
-			fflush(stdin);
+			fseek(stdin, 0, SEEK_END);
 
-			printf("Enter address: ");
-			fflush(stdin);
+			printf("\tCOMPLETE ADDRESS OF POLICE STATION: ");
 			gets(record.address);
-			fflush(stdin);
+			fseek(stdin, 0, SEEK_END);
 
 			fwrite(&record, sizeof(record), 1, fp);
-			printf("Your record is added!");
+			printf("\nYOUR RECORD IS ADDED...\n");
+
 		}
 
-		printf("Add another record? (Y/N)");
-		Y = _getch();
+		printf("\n\tADD ANOTHER RECORD...(Y/N) \t");
+		fseek(stdin, 0, SEEK_END);
+		Y = getch();
+
 	}
-	
+
 	fclose(fp);
-	printf("\nEnter any key to exit.....");
-	_getch();
+	printf("\n\n\tPRESS ANY KEY TO EXIT...");
+	getch();
+
 }
-*/
 
 void viewrecord()
 {
 		system("cls");
 		FILE* fp;
-		//struct record a;
 		char filename[30];
 
 		printf("\n\n\t\t====================================\n");
 		printf("\t\t\t - LIST RECORDS -");
 		printf("\n\t\t====================================\n");
 
-		fp = fopen("file", "rb");
+		fp = fopen("filename", "rb");
 		rewind(fp);
 		while ((fread(&record, sizeof(record), 1, fp)) == 1)
 		{
 			printf("\n\n\t\t::PRESS ENTER TO VIEW MORE RECORDS!::\n");
 			printf("\nCONVICT'S NAME IS: %s", record.name);
 			printf("\nCONVICT'S SEX IS: %s", record.gender);
-			//printf("\nCONVICT'S AGE IS: %s", .age);
 			printf("\nCONVICT'S WEIGHT IS: %s", record.weight);
 			printf("\nCONVICT'S HEIGHT IS: %s", record.height);
 			printf("\nCONVICT'S HAIRCOLOR IS: %s", record.hair);
 			printf("\nCONVICT'S EYECOLOR IS: %s", record.eye);
 			printf("\nCONVICT'S CRIME IS: %s", record.crime);
-			//printf("\nFACE DETAILS: %s", a.details);
-			//printf("\nCOURT IS: %s", a.court);
-			//printf("\nACT: %s", a.act);
-			//printf("\nCONVICT'S CONVICTION IS: %s", a.punishment);
 			printf("\nADDRESS OF POLICE STATION: %s", record.address);
-			//printf("\nFIR NUMBER: %s", a.fir);
-			//printf("\nCONVICT'S EMERGENCY CONTACT IS: %s", a.emergencyc);
-			//printf("\nRELATION OF EMERGENCY CONTACT WITH CONVICT IS: %s", a.emergencyr);
-			//printf("\nARRESTED DATE: %s", a.datearr);
-			_getch();
+			getch();
 
 		}
 		fclose(fp);
-		_getch();
+		getch();
 }
